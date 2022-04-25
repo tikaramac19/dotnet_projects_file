bec171443af8ab77a03ce0d98fe1ab2cc9bb4d4a
tikaramac19 <tikaramac19@gmail.com>
2022-04-25 20:30:28 +0545

    working with date  and time

--- dayFirst/Ncc.first.app/Program.cs
+++ dayFirst/Ncc.first.app/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main()
+        {
+                Console.WriteLine("What is your name?");
+
+                var fullname = Console.ReadLine();
+                var currentDate = DateTime.Now;
+                Console.WriteLine($"{Environment.NewLine} Hello,  {fullname}, on {currentDate} at {currentDate}");
+                Console.Write($"{Environment.NewLine} press any key to exit...");
+                Console.ReadKey(true);            
+                                                                                
+        }
+    }
+}
