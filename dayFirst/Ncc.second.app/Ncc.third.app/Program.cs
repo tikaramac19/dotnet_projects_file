35dc9653a7ddfeb0f2a5d3e48c67dce148300e19
tikaramac19 <tikaramac19@gmail.com>
2022-04-26 10:21:10 +0545

    conditional statement

---
+++ dayFirst/Ncc.second.app/Ncc.third.app/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConditionalStatement
+{
+    class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("If else concept: ");
+
+            var num1 = 20;
+            var num2 = 30;
+            var num3 = 10;
+
+            if(num1 >= num2 && num1 >= num3){
+                Console.WriteLine("Num1 is greater than others");
+            }
+            else if(num2>= num1 && num2 >= num3){
+
+                    Console.WriteLine("num2 is greater than others");
+            }
+            else{
+                Console.WriteLine("num3 is greater than others");
+            }
+
+            ConditionalStatement();
+            return;
+
+        }
+        static void ConditionalStatement ()
+        {
+            Console.WriteLine("Enter the day: ");
+            var choice = Convert.ToInt32(Console.ReadLine());
+
+            string day = "";
+
+            if(choice == 1){
+                day = "sunday";
+            }
+            else if(choice == 2){
+                day = "mondey";
+            }
+            else if(choice == 3){
+                day = "tuesday";
+            }
+            else if(choice ==4 ){
+                day = "wednersday";
+            }
+            else if(choice ==5){
+                day = "thursday";
+            }
+            else if(choice == 6){
+                day ="friday";
+            }
+            else{
+                day= "saturday";
+            }
+
+            Console.WriteLine(day);
+        }
+        static void switchStatement ()
+        {
+            var choice1 = Convert.ToInt32(Console.ReadLine());
+            string dat1 = "";
+
+            switch(choice1)
+            {
+               
+            }
+        }
+
+    }
+}
