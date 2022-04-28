339922977596800374df2364e2942ad55449b0bc
tikaramac19 <tikaramac19@gmail.com>
2022-04-28 17:08:31 +0545

    setup

---
+++ daythird/Ncc.userInput/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Ncc.userInput
+{
+    class program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("lets learn details about user Input ! ");
+
+            userInput();
+
+            return;
+        }
+        public static void userInput()
+        {
+                //get user input
+        }
+    }
+}
