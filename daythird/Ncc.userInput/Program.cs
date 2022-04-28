71348955d0266bc9adb76131ae8dffcd42b34ccb
tikaramac19 <tikaramac19@gmail.com>
2022-04-28 18:48:07 +0545

    userinput added

--- daythird/Ncc.userInput/Program.cs
+++ daythird/Ncc.userInput/Program.cs
@@ -17,6 +17,50 @@ namespace Ncc.userInput
         public static void userInput()
         {
                 //get user input
+
+                // we have already learned that Console.WriteLine()is used to print values. 
+                // Now we will use Console.ReadLine() to get user input.
+
+                //example
+
+                Console.WriteLine("Enter your First Name : "); // type your first name and press enter
+
+                // creates a string variable and get user input from the keyboard and store it in the variable.
+                string fname = Console.ReadLine();
+
+                // Console.WriteLine($"{Environment.NewLine}");
+
+                Console.WriteLine("Enter your Last Name: ");
+
+                string lname = Console.ReadLine();
+
+                // Console.WriteLine($"{Environment.NewLine}");
+
+                string fullname = fname + " " + lname;
+                Console.WriteLine($"Your fullname is :{fullname}");
+
+            // User input and Numbers
+            
+            // The Console.ReadLine() method returns a string. 
+            //Therefore, we cannot get information from another data types, such as int.
+            //the following example shows an error
+
+/*
+            Console.WriteLine("Enter your age : ");
+            int age = Console.ReadLine(); // This is invalid
+
+            Console.WriteLine("The age is " + age);
+*/
+
+            // Like the error says, we cannot implicity convert type 'string' to 'int'.
+
+            //solution
+
+            Console.WriteLine("Enter your age : ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Your age is : {age}");
         }
     }
 }
