799180974ab19d8bb8bba6a6c623de0e38d7ebd0
tikaramac19 <tikaramac19@gmail.com>
2022-04-30 07:54:38 +0545

    methods in c#

---
+++ daythird/Ncc.methods/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+
+namespace methods
+{
+    class program
+    {
+        public static void Main()
+        {
+                myMethod();
+                myMethod();
+                myMethod();
+                myMethod();
+                return;
+        }
+        static void myMethod()
+        {
+                Console.WriteLine("Namaste Hello");
+        }
+    }
+}
