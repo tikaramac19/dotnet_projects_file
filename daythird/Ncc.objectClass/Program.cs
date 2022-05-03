8678890a599cdb42b93f6d0c71ac4e8bd241c258
tikaramac19 <tikaramac19@gmail.com>
2022-05-03 20:20:30 +0545

    modified

--- daythird/Ncc.objectClass/Program.cs
+++ daythird/Ncc.objectClass/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace ObjectClass
+{
+    class progrem
+    {
+        public static void Main(string[] args)
+        {
+                Console.WriteLine("About class and object in c#");
+            
+            // Object -oriented programming has several advantages over procedural programming 
+            // OOP is faster and easier to execute
+            // OOP helps to keep the c# code DRY " dont repeat yourself", and makes the code easier to maintain, modify and debug
+            // OOP provides a clear structure for the programs
+            // OOP makes it possible to create full reusable applications with less code and shorter development time
+
+            // Classes and objects are the two main aspects of object-oriented programming.
+            // Class: fruits, objects: apple, banana, mango etc.
+            // class: car,  objects: olvo , audi , toyota etc.
+
+            // so , a class is a template for objects, and an object is an instance of a class.      
+        }
+
+       
+    }
+}
---
+++ daythird/Ncc.objectClass/objectClass.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ObjectClass
+{
+    public class car
+    {
+        // classes and objects
+        // Everything in c# is associated with classes and objects, alogn with its attributes and methods.
+
+        // TO create a class , use class keyword.
+        // Eg: class Car {
+                    // string color = "red";
+        // }
+        
+    }
+}
