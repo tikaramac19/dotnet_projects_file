22c1326060ce07eb871930fcefbb3c08f804308c
tikaramac19 <tikaramac19@gmail.com>
2022-05-03 21:14:14 +0545

    contructor ko kura

--- daythird/Ncc.objectClass/Program.cs
+++ daythird/Ncc.objectClass/Program.cs
@@ -6,10 +6,18 @@ namespace ObjectClass
 {
     class Program
     {
+        string books ;
+        string mname ;
+        // creating a class constructor with single parameter
+        public Program(string middleName){ // passing parameter in constructor 
+            books = "Bhagwat Geeta";
+
+            mname = middleName;
+        }
         public static void Main(string[] args)
         {
-                Console.WriteLine("About class and object in c#");
-            
+            Console.WriteLine("About class and object in c#");
+
             // Object -oriented programming has several advantages over procedural programming 
             // OOP is faster and easier to execute
             // OOP helps to keep the c# code DRY " dont repeat yourself", and makes the code easier to maintain, modify and debug
@@ -23,9 +31,9 @@ namespace ObjectClass
             // so , a class is a template for objects, and an object is an instance of a class.
 
             car myobj = new car();
-            
+
             Console.WriteLine(myobj.color);
-            
+
             Console.WriteLine(myobj.speed);
 
             Console.WriteLine(myobj.weight);
@@ -41,10 +49,53 @@ namespace ObjectClass
 
             myobj.fullname(myobj.fname, myobj.lname);
 
-            
+            // Constructors
+            // A constructor is a special method that is used to initialize objects. The advantage of 
+            // a constructor is that it is called when an object of a class is created. It can be used to set
+            // initial values for fields.
 
+            // Create a Car class
+            //class Car
+            //{
+            //public string model;  // Create a field
+
+            // Create a class constructor for the Car class
+            //public Car()
+            //{
+            //model = "Mustang"; // Set the initial value for model
+            //}
+
+            //static void Main(string[] args)
+            //{
+            // Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
+            // Console.WriteLine(Ford.model);  // Print the value of model
+            //}
+            //}
+
+            // Outputs "Mustang"
+
+            // creating objecs of program class
+
+            Program obj2 = new Program("Prasad");
+
+            Console.WriteLine(obj2.books);
+
+            //Note : Constructor name must match the class name, and it cannot have a return type (like void or int);
+            // Constructor is called when the object is created 
+            // All classes have constructors by default: if you do not create a class constructor yourself, 
+            // C# creates one for you. However, then you are not able to set initial values for fields.
+            // Constructors save time.
+
+            // Constructor parameters
+            // Constructors can also take parameters, which is used to initialize fields.
+
+            Console.WriteLine(obj2.mname);
+
+            // Note: You can have as many parameters as you want.
+
+            // JUst like other methods, constructors can be overloaded by using different numbers of parameters.
         }
 
-       
+
     }
 }
