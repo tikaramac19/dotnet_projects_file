143c40d11a60d0264a3a331830d3d6ffa03954dc
tikaramac19 <tikaramac19@gmail.com>
2022-05-04 09:39:39 +0545

    constructor and dustructor

---
+++ dayfour/Ncc.object/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Object
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("About object and class");
+        }
+    }
+}
