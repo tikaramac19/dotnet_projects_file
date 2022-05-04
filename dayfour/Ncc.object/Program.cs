6202c1bc4e836e9cb6ade70bdfc4a52e581f5de2
tikaramac19 <tikaramac19@gmail.com>
2022-05-04 10:17:02 +0545

    static and non-static

--- dayfour/Ncc.object/Program.cs
+++ dayfour/Ncc.object/Program.cs
@@ -3,12 +3,42 @@
 using System;
 
 namespace Object
-{
+{ 
     class Program
     {
         public static void Main()
         {
-            Console.WriteLine("About object and class");
+            Console.WriteLine("About Static and non-static class");
+
+            // staticClass objOne = new staticClass();
+            // This is not valid because staticClass itself a object
         }
+    }
+
+    public class NonStaticClass{
+            public int i = 10;
+
+        public void functionOne(){
+
+            // public NonStaticClass(){
+
+            // }
+
+        }
+        
+    }
+        //for static classs , all the members should be static like properties, methods , variables , 
+        // constructor , distructor etc.
+    public static class staticClass{
+        // public void functionOne() is not allowed
+
+        public static int i = 10;
+        public static void functionOne(){
+
+            // static staticClass(){
+                
+            // }
+        }
+
     }
 }
