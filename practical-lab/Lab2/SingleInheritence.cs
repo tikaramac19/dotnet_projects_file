56492bd8672f7c32f68c63eee1f2c5c982078d93
tikaramac19 <tikaramac19@gmail.com>
2022-08-20 19:54:37 +0545

    single inheritence

---
+++ practical-lab/Lab2/SingleInheritence.cs
@@ -0,0 +1,15 @@
+namespace Ncc.Lab.two
+{
+    public class Animal 
+    {
+        public void Eat(){
+            Console.WriteLine("Animal Can Eat.");
+        }
+    }
+    public class Cat:Animal
+    {
+        public void Color(){
+            Console.WriteLine("A color of my cat is Gray. ");
+        }
+    }
+}
