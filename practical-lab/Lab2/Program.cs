e55f7e755f8b9079a315853ab8e971fe95ac7c06
tikaramac19 <tikaramac19@gmail.com>
2022-08-20 19:34:23 +0545

    lab one class and object

---
+++ practical-lab/Lab2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ncc.Lab.two
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello there, its lab 2");
+
+            // creating object of class Introduction
+
+            Introduction obj1 = new Introduction();
+            
+            obj1.FullName("Tikaram", "Acharya");
+
+        }
+
+
+    }
+
+    // creating class called introduction
+    public class Introduction
+    {
+        public  void FullName(string fname, string lname)
+        {
+
+            string fullname = fname + " " + lname;
+
+            Console.WriteLine("My name is " + fullname + ".");
+        }
+    }
+
+}
